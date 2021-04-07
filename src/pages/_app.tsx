6d641d9220b85b84336afeb89a4fd63c7a2dad0c
Lucas Eduardo <lucaseduardo.brum123@gmail.com>
2021-04-07 10:29:31 -0300

    Término da aplicação + deploy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,9 @@ import {ChallengesProvider} from '../contexts/ChallengesContext'
 
 function MyApp({ Component, pageProps }) {
   return ( 
-  <ChallengesProvider>
+    
     <Component {...pageProps} /> 
-  </ChallengesProvider>
+    
   )
 }
 
